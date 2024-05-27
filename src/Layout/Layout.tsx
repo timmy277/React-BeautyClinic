f117483faffcd904697638c1f235b3162ac756c2
timmy277 <140733715+timmy277@users.noreply.github.com>
2024-05-27 10:42:24 +0700

    fix depend

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = () => {
         <>
             <NavigationBar />
                 <Routes>
-                    <Route path="/Home1" element={<Home1 />} />
+                    <Route index path="/Home1" element={<Home1 />} />
                     <Route path="/Home2" element={<Home2 />} />
                 </Routes>
             <Footer />
