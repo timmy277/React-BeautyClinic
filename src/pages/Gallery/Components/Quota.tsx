716dc37f879e007a5bcea5125f204551753fc108
Timmy <140733715+timmy277@users.noreply.github.com>
2024-06-07 08:57:54 +0700

    add quota

--- src/pages/Gallery/Components/Quota.tsx
+++ src/pages/Gallery/Components/Quota.tsx
@@ -1,17 +1,25 @@
+import tw from'twin.macro'
+import { GrayP, TwButton, TwSpan, TwTitle_MD } from '../../../components/Material'
 
 const Quota = () => {
+    const QuotaContainer = tw.div`max-w-[71.25rem] flex mx-auto pt-[7.5rem] items-center justify-between`
+    const QuotaText = tw.div`max-w-[55%]`
+    const QuotaSpan = tw(TwSpan)``
+    const QuotaTitle = tw(TwTitle_MD)`mt-[0.6rem] mb-[1.38rem]  tracking-[0.008rem]`
+    const QuotaDescription = tw(GrayP)`tracking-[0.094rem] max-w-[90%]` 
+    const QuotaButton = tw(TwButton)`mr-[3.6rem] mt-[1rem] mr-[3.2rem] mb-[1.1rem] ml-[3.4rem]`
     return (
         <>
-            <div>
+            <QuotaContainer>
+                <QuotaText>
+                    <QuotaSpan>Get The Quota</QuotaSpan>
+                    <QuotaTitle>Want to be handled by our professional team immediately?</QuotaTitle>
+                    <QuotaDescription>Id dui erat sed quam tellus in purus. Pellentesque congue fringilla cras tellus enim.</QuotaDescription>
+                </QuotaText>
                 <div>
-                    <span>Get The Quota</span>
-                    <h1>Want to be handled by our professional team immediately?</h1>
-                    <p>Id dui erat sed quam tellus in purus. Pellentesque congue fringilla cras tellus enim.</p>
+                    <QuotaButton>Make an Appointment</QuotaButton>
                 </div>
-                <div>
-                    <button></button>
-                </div>
-            </div>
+            </QuotaContainer>
         </>
     )
 }
