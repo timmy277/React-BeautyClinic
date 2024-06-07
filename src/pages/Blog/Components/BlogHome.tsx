9fad89cda238bc3d6b562516604951ac24f1fe80
Timmy <140733715+timmy277@users.noreply.github.com>
2024-06-07 17:57:10 +0700

    responsive blog  home component

--- src/pages/Blog/Components/BlogHome.tsx
+++ src/pages/Blog/Components/BlogHome.tsx
@@ -6,8 +6,8 @@ const BlogHome = () => {
     const Overlay = tw.div`top-0 bottom-0 left-0 right-0 absolute z-0 bg-[#09115699] opacity-[1.6]`;
 
     const HomeContainer = tw.div`flex  items-center mx-auto z-10 absolute inset-0 `    
-    const HomeTitle = tw(TwTitle_MD)`text-white max-w-[50%] mx-auto ml-[9.3rem] mb-[-0.4rem] tracking-[0.013rem] 2lg:ml-auto lg:ml-auto md:mb-[0.8rem] md:ml-auto sm:mb-[0.4rem] sm:ml-auto xs:ml-auto xs:mb-0 xs:max-w-[80%]`
-    const HomeP = tw(GrayP)`font-medium text-[#D9D9D9] mr-[9.2rem] mt-[0.2rem]`
+    const HomeTitle = tw(TwTitle_MD)`text-white max-w-[50%] mx-auto ml-[9.3rem] mb-[-0.4rem] tracking-[0.013rem] sm:ml-[15%]`
+    const HomeP = tw(GrayP)`font-medium text-[#D9D9D9] mr-[9.2rem] mt-[0.2rem] sm:mr-[15%]`
     return (
         <>
         <HomeBackground>
