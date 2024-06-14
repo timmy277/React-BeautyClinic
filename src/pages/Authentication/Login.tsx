4485e1437131e27500897ba1b26962df1ce15133
Timmy <140733715+timmy277@users.noreply.github.com>
2024-06-14 13:43:27 +0700

    add nagivate

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import tw from "twin.macro";
 import { GrayP, TwButton, TwTitle_LG } from "../../components/Material";
 import { Link, useNavigate } from "react-router-dom";
 import { fetchSignInMethodsForEmail, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useLocation } from 'react-router-dom';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState("");
@@ -13,16 +16,28 @@ const Login = () => {
     const navigate = useNavigate();
     const [authing, setAuthing] = useState(false);
     const [errors, setErrors] = useState<{ email?: string, password?: string }>({});
+    const [isLoginSuccessful, setIsLoginSuccessful] = useState(false);
+    const location = useLocation();
 
     const LoginTitle = tw(TwTitle_LG)`text-center text-light_pink mb-[4rem]`
     const Icon = tw.div`absolute right-[3.6rem] top-[3.6rem] transform -translate-y-1/2 cursor-pointer`;
 
-    const LoginP = tw(GrayP)`text-center mb-4`
+    const LoginP = tw(GrayP)`text-center mb-4 mt-8`
     const LoginSpan = tw.span`text-dark_blue`
-    const LoginButton = tw(TwButton)`block mx-auto px-[45%] mb-8 h-[4rem]`
-    const LoginGoogle = tw(LoginButton)`text-nowrap px-[33%] flex items-center gap-4`
+    const LoginButton = tw(TwButton)`block mx-auto w-full mb-8 h-[4rem]`
+    const LoginGoogle = tw(LoginButton)`text-nowrap w-full flex items-center justify-center gap-4 text-center`
+    const ErrorP = tw(GrayP)`text-red-500 ml-[1.5rem]`
 
 
+
+    useEffect(() => {
+        if (location.state) {
+            setEmail(location.state.email);
+            setPassword(location.state.password);
+        }
+    }, [location]);
+
+    
     const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault();
         const formErrors: { email?: string, password?: string } = {};
@@ -33,29 +48,37 @@ const Login = () => {
         }
         if (!email) {
             formErrors.email = 'Email is required';
-        } else if (!/^\S+@\S+$/i.test(email)) {
+        }
+        else if (!/^\S+@\S+$/i.test(email)) {
             formErrors.email = 'Entered value does not match email format';
-        } else if (email.length > 20) {
+        }
+        else if (email.length > 20) {
             formErrors.email = 'Email must be less than 20 characters';
         }
-
         if (!password) {
             formErrors.password = 'Password is required';
-        } else if (password.length < 8) {
+        }
+        else if (password.length < 8) {
             formErrors.password = 'Password must be at least 8 characters';
         }
-
-
+        else if (!/[A-Z]/.test(password)){
+            formErrors.password = 'Password must contain at least one uppercase letter';
+        }
+        // else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)){
+        //     formErrors.password = 'Password must contain at least one special character';
+        // }
         setErrors(formErrors);
 
         if (Object.keys(formErrors).length === 0) {
             try {
                 await signInWithEmailAndPassword(auth, email, password);
-                alert('Logged in successfully!');
+                setIsLoginSuccessful(true);
+                // alert('Logged in successfully!');
                 navigate('/');
             } catch (error) {
                 console.error(error);
-                alert('Email or password is incorrect');
+                // alert('Email or password is incorrect');
+                setIsLoginSuccessful(false);
             }
         }
     };
@@ -71,43 +94,46 @@ const Login = () => {
             setAuthing(false);
         });
     }
+    // console.log(isLoginSuccessful);
 
-    return (     
-        <div tw='max-w-[50rem] bg-white shadow-md mx-auto mt-[2%] px-[3%] pt-[5%] pb-[5%] rounded-[3rem]'>
-            <form onSubmit={handleLogin}>
-                <LoginTitle>Login</LoginTitle>
-                <label tw='mb-[-2rem] ml-[1.5rem] text-light_pink'>Email</label>
-                <input tw='border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem]    text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full w-full mb-[2. 395rem] items-center'
-                type="email" value={email} onChange={(e) => setEmail(e.target.value)}
-                name="" id="" placeholder="Please enter your email" />
-                {errors.email && <p tw='text-red-500'>{errors.email}</p>}
-
-                <div tw='relative mt-8'>
-                    <label tw='ml-[1.5rem] text-light_pink' htmlFor="pass">Password</label>
-                    <input tw='w-full px-4 py-2 border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full mb-[2.395rem] items-center'
-                    // {...register("password",{ required: true, minLength: 8})}
-                    id="pass" type={showPassword ? "text" : "password"} placeholder="Please enter your password" 
-                    value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    {errors.password && <p tw='text-red-500'>{errors.password}</p>}
-                    <Icon onClick={() => setShowPassword((prev) => !prev) }>
-                        {showPassword ? <FaEye tw='w-8 h-8' /> : <FaEyeSlash tw='w-8 h-8' />}
-                    </Icon>
-                </div>
-                <LoginP>You don't have an account?
-                    <LoginSpan>
-                        <Link to="/Register"> Register</Link>
-                    </LoginSpan>
-                </LoginP>
-                <div tw= 'w-full'>  
-                    <LoginButton type="submit">
-                        {/* {isSignIn ? 'Loading...' : 'Login'} */}Login
-                    </LoginButton>
-                    <LoginGoogle type="submit" disabled={authing} onClick={() => signInWithGoogle()}>
-                        <FcGoogle tw='w-8 h-8 mx-auto' />  Login with Google
-                    </LoginGoogle>
-                </div>
-            </form>
-        </div>
+    return (
+        <div tw='w-full max-w-full 2lg:px-[20%] lg:px-[20%] md:px-[10%] sm:px-[10%]'>  
+            <div tw='max-w-[50rem] bg-white shadow-md mx-auto mt-[2%] px-[3%] pt-[5%] pb-[5%] rounded-[3rem] md:mt-[5%] sm:mt-[5%]'>
+                <form onSubmit={handleLogin}>
+                    <LoginTitle >Login</LoginTitle>
+                    <label tw='mb-[-2rem] ml-[1.5rem] text-light_pink'>Email</label>
+                    <input tw='border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] 
+                    pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full    w-full mb-[2. 395rem] items-center'
+                    type="email" value={email} onChange={(e) => setEmail(e.target.value)}
+                    name="" id="" placeholder="Please enter your email" />
+                    {errors.email && <ErrorP>{errors.email}</ErrorP>}
+                    <div tw='relative mt-8'>
+                        <label tw='ml-[1.5rem] text-light_pink' htmlFor="pass">Password</label>
+                        <input tw='w-full px-4 py-2 border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem]   pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm    sm:text-xs max-w-full items-center'
+                        id="pass" type={showPassword ? "text" : "password"} placeholder="Please enter your password" 
+                        value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        {errors.password && <ErrorP>{errors.password}</ErrorP>}
+                        <Icon onClick={() => setShowPassword((prev) => !prev) }>
+                            {showPassword ? <FaEye tw='w-8 h-8' /> : <FaEyeSlash tw='w-8 h-8' />}
+                        </Icon>
+                    </div>
+                    <LoginP>You don't have an account?
+                        <LoginSpan>
+                            <Link to="/Register"> Register</Link>
+                        </LoginSpan>
+                    </LoginP>
+                    <div tw= 'w-full'>  
+                        <LoginButton type="submit" onClick={() => toast(isLoginSuccessful? 'Logged in successfully!' : 'Email or password is incorrect')}>
+                            Login
+                        </LoginButton>
+                        <LoginGoogle type="submit" disabled={authing} onClick={() => signInWithGoogle()}>
+                            <FcGoogle tw='w-8 h-8' />  Login with Google
+                        </LoginGoogle>
+                    </div>
+                    <ToastContainer />
+                </form>
+            </div>
+        </div>  
     )
 }
 
--- src/pages/Authentication/Register.tsx
+++ src/pages/Authentication/Register.tsx
@@ -8,29 +8,29 @@ import { getAuth } from "firebase/auth";
 import { fetchSignInMethodsForEmail } from 'firebase/auth';
 
 const Register = () => {
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState("");
     const [cfPassword, setCfPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
     const [showCfPassword, setShowCfPassword] = useState(false);
-    const [errors, setErrors] = useState<{ email?: string, password?: string,  cfPassword?: string }>({});
+    const [errors, setErrors] = useState<{username?: string, email?: string, password?: string,  cfPassword?: string }>({});
     const auth = getAuth();
     const navigate = useNavigate();
-
     const RegisterTitle = tw(TwTitle_LG)`text-center text-light_pink mb-[4rem]`
     const Icon = tw.div`absolute right-[3.6rem] top-[3.6rem] transform -translate-y-1/2 cursor-pointer`;
 
     const RegisterP = tw(GrayP)`text-center mb-4`
     const RegisterSpan = tw.span`text-dark_blue`
-    const RegisterButton = tw(TwButton)`block mx-auto px-[45%] mb-8`
-
+    const RegisterButton = tw(TwButton)`block mx-auto w-full mb-8 h-[4rem]`
+    const ErrorP = tw(GrayP)`text-red-500 ml-[1.5rem] mb-4`
 
     const handleSignUp = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        const formErrors: { email?: string, password?: string,  cfPassword?: string } = {};
-
-
+        const formErrors: { username?: string, email?: string, password?: string,  cfPassword?: string } = {};
+        if (!username) {
+            formErrors.username = 'Username is required';
+        } 
         if (!email) {
             formErrors.email = 'Email is required';
         } else if (!/^\S+@\S+$/i.test(email)) {
@@ -38,17 +38,31 @@ const Register = () => {
         } else if (email.length > 20) {
             formErrors.email = 'Email must be less than 20 characters';
         }
-
         if (!password) {
             formErrors.password = 'Password is required';
         } else if (password.length < 8) {
             formErrors.password = 'Password must be at least 8 characters';
         }
 
+        else if (!/[A-Z]/.test(password)){
+            formErrors.password = 'Password must contain at least one uppercase letter';
+        }
+        else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)){
+            formErrors.password = 'Password must contain at least one special character';
+        }
+
+
         if (!cfPassword) {
             formErrors.cfPassword = 'Password is required';
         } else if (cfPassword.length < 8) {
             formErrors.cfPassword = 'Password must be at least 8 characters';
+        }
+
+        else if (!/[A-Z]/.test(cfPassword)){
+            formErrors.cfPassword = 'Password must contain at least one uppercase letter';
+        }
+        else if (!/[!@#$%^&*(),.?":{}|<>]/.test(cfPassword)){
+            formErrors.cfPassword = 'Password must contain at least one special character';
         }
 
         if(password!==cfPassword){
@@ -66,7 +80,8 @@ const Register = () => {
                 }
                 await createUserWithEmailAndPassword(auth, email, password);
                 alert('User created successfully!');
-                navigate('/Login');
+                navigate('/Login', { state: { email, password } });
+
             } catch (error) {
                 console.error(error);
                 alert('Error creating user');
@@ -75,46 +90,54 @@ const Register = () => {
     };
 
     return (
-        <div tw='max-w-[50rem] bg-white shadow-md mx-auto mt-[2%] px-[3%] pt-[5%] pb-[5%] rounded-[3rem]'>
-            <form onSubmit={handleSignUp}  autoComplete="off">
-            <RegisterTitle>Register</RegisterTitle>
-                <label tw='mb-[-2rem] ml-[1.5rem] text-light_pink'>Email</label>
-                <input tw='border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full w-full items-center' autoComplete="off"
-                type="email" value={email} onChange={(e) => setEmail(e.target.value)}
-                name="" id="" placeholder="Please enter your email" />
-                {errors.email && <p tw='text-red-500'>{errors.email}</p>}
+        <div tw='w-full max-w-full 2lg:px-[20%] lg:px-[20%] md:px-[10%] sm:px-[10%]'>  
+            <div tw='max-w-[50rem] bg-white shadow-md mx-auto mt-[2%] px-[3%] pt-[5%] pb-[5%] rounded-[3rem] md:mt-[5%] sm:mt-[5%]'>
+                <form onSubmit={handleSignUp}  autoComplete="off">
+                    <RegisterTitle>Register</RegisterTitle>
+                    
+                    <label tw='mb-[-2rem] ml-[1.5rem] text-light_pink'>Username</label>
+                    <input tw='border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full   w-full items-center' autoComplete="off"
+                    type="text" value={username} onChange={(e) => setUsername(e.target.value)}
+                    name="" id="" placeholder="Please enter your name" />
+                    {errors.username && <ErrorP>{errors.username}</ErrorP>}
 
-                <div tw='relative mt-8'>
-                    <label tw='ml-[1.5rem] text-light_pink' htmlFor="pass" >Password</label>
-
-                    <input tw='w-full px-4 py-2 border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full mb-[1rem] items-center' 
-                    id="pass" type={showPassword ? "text" : "password"} placeholder="Please enter your password" 
-                    value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    {errors.password && <p tw='text-red-500'>{errors.password}</p>}
-                    <Icon onClick={() => setShowPassword((prev) => !prev) }>
-                        {showPassword ? <FaEye tw='w-8 h-8' /> : <FaEyeSlash tw='w-8 h-8' />}
-                    </Icon>
-                </div>
-                <div tw='relative mt-0'>
-                    <label tw='ml-[1.5rem] text-light_pink' htmlFor="confirmPass">Password</label>
-
-                    <input tw='w-full px-4 py-2 border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full mb-[1rem] items-center' 
-                    id="confirmPass" type={showCfPassword ? "text" : "password"} placeholder="Please confirm your password" 
-                    value={cfPassword} onChange={(e) => setCfPassword(e.target.value)}/>
-                    {errors.cfPassword && <p tw='text-red-500'>{errors.cfPassword}</p>}
-                    <Icon onClick={() => setShowCfPassword((prev) => !prev) }>
-                        {showCfPassword ? <FaEye tw='w-8 h-8' /> : <FaEyeSlash tw='w-8 h-8' />}
-                    </Icon>
-                </div>
-                <RegisterP>Already have an account?
-                    <RegisterSpan>
-                        <Link to="/Login">Login</Link>
-                    </RegisterSpan>
-                </RegisterP>
-                <div tw= 'w-full'>
-                    <RegisterButton type="submit">Register</RegisterButton>
-                </div>
-            </form>
+                    <div tw='mt-8'>
+                        <label tw='mb-[-2rem] ml-[1.5rem] text-light_pink'>Email</label>
+                        <input tw='border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full   w-full items-center' autoComplete="off"
+                        type="email" value={email} onChange={(e) => setEmail(e.target.value)}
+                        name="" id="" placeholder="Please enter your email" />
+                        {errors.email && <ErrorP>{errors.email}</ErrorP>}
+                    </div>
+                    <div tw='relative mt-8'>
+                        <label tw='ml-[1.5rem] text-light_pink' htmlFor="pass" >Password</label>
+                        <input tw='w-full px-4 py-2 border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem]   pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm    sm:text-xs max-w-full items-center' 
+                        id="pass" type={showPassword ? "text" : "password"} placeholder="Please enter your password" 
+                        value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        {errors.password && <ErrorP>{errors.password}</ErrorP>}
+                        <Icon onClick={() => setShowPassword((prev) => !prev) }>
+                            {showPassword ? <FaEye tw='w-8 h-8' /> : <FaEyeSlash tw='w-8 h-8' />}
+                        </Icon>
+                    </div>
+                    <div tw='relative mt-8'>
+                        <label tw='ml-[1.5rem] text-light_pink' htmlFor="confirmPass">Password</label>
+                        <input tw='w-full px-4 py-2 border border-solid h-[3.849rem] border-[#D9DDFE] rounded-2xl pt-[1.063rem] pr-[0rem] pb-[1.1rem] pl-[1.5rem] text-dark_blue font-poppins text-base leading-6 tracking-widest font-normal md:text-sm sm:text-xs max-w-full items-center' 
+                        id="confirmPass" type={showCfPassword ? "text" : "password"} placeholder="Please confirm your password" 
+                        value={cfPassword} onChange={(e) => setCfPassword(e.target.value)}/>
+                        {errors.cfPassword && <ErrorP>{errors.cfPassword}</ErrorP>}
+                        <Icon onClick={() => setShowCfPassword((prev) => !prev) }>
+                            {showCfPassword ? <FaEye tw='w-8 h-8' /> : <FaEyeSlash tw='w-8 h-8' />}
+                        </Icon>
+                    </div>
+                    <RegisterP>Already have an account?
+                        <RegisterSpan>
+                            <Link to="/Login">Login</Link>
+                        </RegisterSpan>
+                    </RegisterP>
+                    <div tw= 'w-full'>
+                        <RegisterButton type="submit">Register</RegisterButton>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
